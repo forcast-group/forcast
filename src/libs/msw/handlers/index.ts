60dba14195f180b663253efd226e3c652938d26e
Yusuke Mizuno <38448411+hairihou@users.noreply.github.com>
2024-09-23 11:08:46 +0900

    fix(): create barrel

--- src/libs/msw/handlers/index.ts
+++ src/libs/msw/handlers/index.ts
@@ -1,4 +1,4 @@
 import { type HttpHandler } from 'msw';
-import { usersHandlers } from './handlers/users';
+import { usersHandlers } from './users';
 
 export const handlers: HttpHandler[] = [...usersHandlers];
